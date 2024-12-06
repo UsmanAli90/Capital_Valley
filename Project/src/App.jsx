24ef3a77c1fea5c771e561fd15b2c881463ebd2a
UsmanAli90 <usmanali04768@gmail.com>
2024-12-07 00:50:16 +0500

    Implemented frontend functionality

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from './components/Auth/Signin.jsx'
-import Signup from './components/Auth/Signup.jsx'
-import HomePage from './components/HomePage/HomePage.jsx'
+import Signup from './Components/Auth/Signup.jsx'
+import HomePage from './Components/HomePage/HomePage.jsx'
 import Profile from './components/Profile/pages/profile.jsx'
 import ProfileSetting from './components/Profile/pages/profileSetting.jsx'
 import Notfound from './components/Profile/pages/NotFound.jsx'
