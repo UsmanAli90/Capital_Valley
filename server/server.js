6d8c91caf3817928f7861308d388a8b58e60d643
Usman Saleem <chusman3638@gmail.com>
2025-03-05 22:59:47 +0500

    Successfull  Payment Method

--- server/server.js
+++ server/server.js
@@ -144,32 +144,7 @@ app.patch('/posts/:id/upvote', async (req, res) => {
 });
 
 
-// app.post("/api/payment", async (req, res) => {
-//     try {
-//       const { paymentMethodId, amount } = req.body;
-  
-//       // Validate amount to prevent tampering
-//       const validAmounts = [20, 200]; // Allowed amounts in dollars
-//       if (!validAmounts.includes(Number(amount))) {
-//         return res.status(400).json({ success: false, message: "Invalid payment amount" });
-//       }
-  
-//       // Create payment intent
-//       const paymentIntent = await stripe.paymentIntents.create({
-//         amount: amount * 100, // Convert to cents
-//         currency: "usd",
-//         payment_method: paymentMethodId,
-//         confirm: true,
-//         return_url: "http://localhost:3000", // Change this to your actual frontend success page
-//       });
-      
-  
-//       res.json({ success: true, paymentIntent });
-//     } catch (error) {
-//       console.error("Stripe Payment Error:", error);
-//       res.status(400).json({ success: false, message: error.message });
-//     }
-//   });
+
 
 app.post("/startupsignup", createStartup);
 app.post("/investorsignup", createInvestor);
